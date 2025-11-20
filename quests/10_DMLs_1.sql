7f4f17882c3352993e246cac2d4941e8fdd4d969
learnbook <learnbook1103@gmail.com>
2025-11-20 17:06:46 +0900

    10_TP

--- quests/10_DMLs_1.sql
+++ quests/10_DMLs_1.sql
@@ -7,7 +7,7 @@ CREATE TABLE news_articles (
 );
 
 -- 데이터를 테이블에 추가
-INSERT INTO ews_artic (title, url, author, published_at)
+INSERT INTO news_articles (title, url, author, published_at)
 VALUES ('AI 시대 도래', 'https://news.com/ai', '홍길동', '2025-01-01'),
         ('경제 성장률 상승', 'https://news.com/economy', '이영희', '2025-01-05');
 
@@ -19,7 +19,7 @@ WHERE author = '홍길동';
 -- 첫 번째 뉴스 제목을 새로운 문자열로 변경하는 UPDATE문 작성
 UPDATE news_articles
 SET title = 'AI 혁명'
-WHERE personid = 000002;
+WHERE author = '홍길동';
 
 -- 두 번째 뉴스를 삭제하는 DELETE문 작성
 DELETE FROM news_articles
